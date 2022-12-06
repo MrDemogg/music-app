10c535f6435390e20fec77495d786a4ba65191f0
demog <dg08gg@mail.ru>
2022-12-06 21:46:59 +0300

    App.tsx
    Подключил react-router-dom, прописал роуты

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Artists from "./components/Artists";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Albums from "./components/Albums";
+import Tracks from "./components/Tracks";
+import NavBar from "./components/UI/NavBar";
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <NavBar />
+        <div style={{marginTop: 60}}>
+          <Routes>
+            <Route index element={<Artists />} />
+            <Route path={'/artists'} element={<Artists />} />
+            <Route path={'/albums/:id'} element={<Albums />} />
+            <Route path={'/tracks'} element={<Tracks />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
