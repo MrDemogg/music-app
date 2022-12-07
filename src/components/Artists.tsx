25f2e36f3f7371102083e776f155031fa6665ba0
demog <dg08gg@mail.ru>
2022-12-07 19:08:58 +0300

    Artists.tsx
    Использовал вынесенный компонент стилизации, передав всю инфу в него и сократив этот компонент

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import {CardActionArea, CardHeader, CardMedia, IconButton, Typography} from "@mui/material";
+import {CardActionArea, CardHeader, CardMedia, Typography} from "@mui/material";
 import {musicAPI} from "../service/MusicService";
 import {useNavigate} from "react-router-dom";
-import {Cached} from "@mui/icons-material";
-import VisualInfo from "./UI/VisualInfo";
+import Cards from "./UI/Cards";
+import {IArtists} from "../models/IArtists";
 
 const Artists = () => {
   const {data: artists, refetch, status, isLoading, isError} = musicAPI.useFetchArtistsQuery(undefined)
@@ -13,45 +13,26 @@ const Artists = () => {
 
   return (
     <div>
-      {isLoading || isError
-        ? <VisualInfo status={status} isError={isError} isLoading={isLoading} />
-        : artists && artists.length > 0 ?
-          <Card>
-            <div style={{margin: '0 auto', width: 50, display: 'flex'}}>
-              <Typography style={{marginTop: 7}}>Refetch</Typography>
-              <IconButton
-                onClick={() => refetch()}
-                sx={{display: 'flex', alignSelf: 'center'}}
-              >
-                <Cached />
-              </IconButton>
-            </div>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-              {artists.map(artist =>
-              <Card sx={{ width: 300, bgcolor: '#000'}} variant={'outlined'} key={artist._id}>
-                <CardActionArea
-                  onClick={() => router('/albums/' + artist.name)}
-                >
-                  <CardHeader title={artist.name} sx={{textAlign: 'center', color: '#fff'}} />
-                  <CardMedia
-                    component="img"
-                    width={200}
-                    height={200}
-                    image={artist.photo}
-                    alt={artist.name}
-                  />
-                  <CardContent>
-                    <Typography sx={{color: '#fff'}} variant={'subtitle1'}>{artist.info}</Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-              )}
-            </div>
+      <Cards artists={artists && artists} artistElem={
+        (artist: IArtists) =>
+          <Card sx={{ width: 300, bgcolor: '#1d1d1d', marginRight: 3, marginLeft: 3 }} variant={'outlined'} key={artist._id}>
+            <CardActionArea
+              onClick={() => router('/albums/' + artist.name)}
+            >
+              <CardHeader title={artist.name} sx={{textAlign: 'center', color: '#fff'}} />
+              <CardMedia
+                component="img"
+                width={200}
+                height={200}
+                image={artist.photo}
+                alt={artist.name}
+              />
+              <CardContent>
+                <Typography variant="body2" sx={{color: 'rgba(255, 255, 255, 0.7)'}}>{artist.info}</Typography>
+              </CardContent>
+            </CardActionArea>
           </Card>
-          : <Typography variant={'h3'} sx={{textAlign: 'center'}}>
-            В базе данных не было найдено артистов
-          </Typography>
-      }
+      } isError={isError} isLoading={isLoading} status={status} refetch={refetch} />
     </div>
   );
 };
