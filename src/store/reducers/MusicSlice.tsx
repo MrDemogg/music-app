7cf5048feda7cec9725429022e27935403e202e1
demog <dg08gg@mail.ru>
2022-12-08 21:47:33 +0300

    MusicSlice.tsx / слайс
    Добавил свойство globalIsError, для объявленя ошибки глобально в моем проекте (чтобы все компоненты знали про ошибку). Ну и редьюсеры

--- src/store/reducers/MusicSlice.tsx
+++ src/store/reducers/MusicSlice.tsx
@@ -1,12 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 interface MusicState {
-  token: string | null,
-  error: boolean
+  token: string,
+  error: string | null,
+  globalIsError: boolean
 }
 
 const initialState: MusicState = {
-  token: null,
-  error: false,
+  token: '',
+  error: null,
+  globalIsError: false
 }
 
 export const musicSlice = createSlice({
@@ -16,8 +18,11 @@ export const musicSlice = createSlice({
     setToken(state, action: PayloadAction<string>) {
       state.token = action.payload
     },
-    setError(state, action: PayloadAction<boolean>) {
+    setError(state, action: PayloadAction<string | null>) {
       state.error = action.payload
+    },
+    setGlobalIsError(state, action: PayloadAction<boolean>) {
+      state.globalIsError = action.payload
     }
   }
 })
