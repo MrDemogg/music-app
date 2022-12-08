7a3af38bd85595212a0d708513286d2a10aa3536
demog <dg08gg@mail.ru>
2022-12-08 21:33:16 +0300

    Cards.tsx
    Изменил условие, при котором пользователю выводится нашлись ли хоть какие-то данные по запросу или нет

--- src/components/UI/Cards.tsx
+++ src/components/UI/Cards.tsx
@@ -5,6 +5,10 @@ import {ITracks} from "../../models/ITracks";
 import VisualInfo from "./VisualInfo";
 import {Card, IconButton, Typography} from "@mui/material";
 import {Cached} from "@mui/icons-material";
+import {SerializedError} from "@reduxjs/toolkit";
+import {FetchBaseQueryError} from "@reduxjs/toolkit/query";
+import {useAppDispatch, useAppSelector} from "../../hooks/redux";
+import {musicSlice} from "../../store/reducers/MusicSlice";
 
 interface CardsProps {
   artists?: IArtists[],
@@ -12,10 +16,10 @@ interface CardsProps {
   tracks?: ITracks[],
   artistElem?: (arg: IArtists) => ReactNode,
   albumsElem?: (arg: IAlbums) => ReactNode,
-  tracksElem?: (arg: ITracks) => ReactNode,
+  tracksElem?: (arg: ITracks, index: number) => ReactNode,
   isError: boolean,
+  error?: SerializedError | FetchBaseQueryError,
   isLoading: boolean,
-  status: string,
   refetch: () => void
 }
 
@@ -26,34 +30,46 @@ const Cards: FC<CardsProps> = ({
   albumsElem,
   artistElem,
   tracksElem,
-  isError,
   isLoading,
-  status,
-  refetch
+  refetch,
+  error
 }) => {
+  const dispatch = useAppDispatch()
+  const {globalIsError} = useAppSelector(state => state.musicReducer)
+  if (globalIsError && error) {
+    if ('error' in error) {
+      dispatch(musicSlice.actions.setError(error.error))
+    }
+  }
   return (
     <div>
-      <VisualInfo status={status} isError={isError} isLoading={isLoading}/>
+      <div style={{margin: '0 auto', width: 50, display: 'flex'}}>
+        <Typography style={{marginTop: 7}}>Refetch</Typography>
+        <IconButton
+          onClick={() => {
+            dispatch(musicSlice.actions.setGlobalIsError(false))
+            dispatch(musicSlice.actions.setError(null))
+            refetch()
+          }}
+          sx={{display: 'flex', justifyContent: 'center'}}
+        >
+          <Cached/>
+        </IconButton>
+      </div>
       {(artists && artists.length > 0) || (albums && albums.length > 0) || (tracks && tracks.length > 0) ?
-        <Card>
-          <div style={{margin: '0 auto', width: 50, display: 'flex'}}>
-            <Typography style={{marginTop: 7}}>Refetch</Typography>
-            <IconButton
-              onClick={() => refetch()}
-              sx={{display: 'flex', justifyContent: 'center'}}
-            >
-              <Cached/>
-            </IconButton>
-          </div>
-          <div style={{display: 'flex', justifyContent: 'center'}}>
-            {albums && albumsElem && albums.map(albumsElem)}
-            {artists && artistElem && artists.map(artistElem)}
-            {tracks && tracksElem && tracks.map(tracksElem)}
-          </div>
-        </Card>
-        : <Typography variant={'h3'} sx={{textAlign: 'center'}}>
-          В базе данных не было найдено артистов
-        </Typography>}
+            <Card>
+              <div style={{display: 'flex', justifyContent: 'center'}}>
+                {albums && albumsElem && albums.map(albumsElem)}
+                {artists && artistElem && artists.map(artistElem)}
+                {tracks && tracksElem && tracks.map(tracksElem)}
+              </div>
+            </Card>
+        :  !isLoading && !globalIsError
+        && <Typography variant={'h3'} sx={{textAlign: 'center'}}>
+          В базе данных не было найдено нужных данных
+        </Typography>
+      }
+      <VisualInfo isLoading={isLoading}/>
     </div>
   );
 };
