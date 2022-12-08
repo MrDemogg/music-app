58d337dd99d5a7d93b87809951616cb2850adc34
demog <dg08gg@mail.ru>
2022-12-08 21:49:56 +0300

    NavBar.tsx
    Сделал общий handler для перемещения между страницами. Вообще в этом не было бы смысло, если бы не функции сброса ошибки

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {useLocation, useNavigate} from "react-router-dom";
+import {useAppDispatch} from "../../hooks/redux";
+import {musicSlice} from "../../store/reducers/MusicSlice";
 
 const NavBar = () => {
   const router = useNavigate()
   const endpoint = useLocation()
+  const dispatch = useAppDispatch()
+
+  const navHandler = (endpoint: string) => {
+    router(endpoint)
+    dispatch(musicSlice.actions.setError(null))
+    dispatch(musicSlice.actions.setGlobalIsError(false))
+  }
 
   return (
     <div>
@@ -12,9 +21,9 @@ const NavBar = () => {
       <Container fluid style={{width: '100%'}}>
         <Navbar.Brand color={'#fff'} style={{width: '51.4%', display: 'flex', justifyContent: 'right'}}>{endpoint.pathname.slice(1, 7)}</Navbar.Brand>
         <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link onClick={() => router('/artists')}>Artists</Nav.Link>
-          <Nav.Link onClick={() => router('/albums')}>Albums</Nav.Link>
-          <Nav.Link onClick={() => router('/tracks')}>Tracks</Nav.Link>
+          <Nav.Link onClick={() => navHandler('/artists')}>Artists</Nav.Link>
+          <Nav.Link onClick={() => navHandler('/albums')}>Albums</Nav.Link>
+          <Nav.Link onClick={() => navHandler('/tracks')}>Tracks</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
