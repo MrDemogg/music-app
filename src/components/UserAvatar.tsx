d0926caac18687d6d39c4c71c92503dfc9ce13a5
demog <dg08gg@mail.ru>
2022-12-09 20:54:14 +0300

    UserAvatar.tsx
    Подключил модальное окно для выхода из профиля

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -3,14 +3,17 @@ import {Avatar, Button} from "@mui/material";
 import {lightBlue, lime, orange, pink, purple, red, teal, yellow} from "@mui/material/colors";
 import {useAppSelector} from "../hooks/redux";
 import ProfileUsageModal from "./ProfileUsageModal";
+import Profile from "./Profile";
 
 const colors = [purple, pink, red, teal, lightBlue, lime, yellow, orange]
 
 const UserAvatar = () => {
   const {username} = useAppSelector(state => state.musicReducer)
-  const [modalVisible, setModalVisible] = useState(false)
+  const [usageModalVisible, setUsageModalVisible] = useState(false)
+  const [profileModalVisible, setProfileModalVisible] = useState(false)
   const [usedColor, setUsedColor] = useState<"#e1bee7" | "#f8bbd0" | "#ffcdd2" | "#b2dfdb" | "#b3e5fc" | "#f0f4c3" | "#fff9c4" | "#ffe0b2">()
-  const modalHandler = () => setModalVisible(!modalVisible)
+  const usageModalHandler = () => setUsageModalVisible(!usageModalVisible)
+  const profileModalHandler = () => setProfileModalVisible(!profileModalVisible)
 
   useEffect(() => {
     setUsedColor(colors[Math.floor(Math.random() * colors.length)][100])
@@ -19,12 +22,17 @@ const UserAvatar = () => {
   return (
     <div>
       {username
-        ? <Avatar sx={{bgcolor: usedColor}}>{username.slice(0, 1)}</Avatar>
-        : <Button variant="outlined" color="info" onClick={modalHandler}>
+        ? <Avatar sx={{bgcolor: usedColor}} onClick={profileModalHandler} style={{cursor: 'pointer'}}>{username.slice(0, 1)}</Avatar>
+        : <Button variant="outlined" color="info" onClick={usageModalHandler}>
             Login / Register
           </Button>
       }
-      <ProfileUsageModal handleChange={modalHandler} visible={modalVisible} />
+      <ProfileUsageModal handleChange={usageModalHandler} visible={usageModalVisible} />
+      <Profile
+        avatar={<Avatar sx={{bgcolor: usedColor}}>{username && username.slice(0, 1)}</Avatar>}
+        visible={profileModalVisible}
+        handleChange={profileModalHandler}
+      />
     </div>
   );
 };
